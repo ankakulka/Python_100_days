b0c4e65f5455618c4c7cced36a4dc49bb95dcc61
ankakulczycka@gmail.com <ankakulczycka@gmail.com>
2023-01-23 19:52:38 +0100

    Updated Days 6 and 10

--- Day_10.py
+++ Day_10.py
@@ -17,3 +17,111 @@ def is_leap(year):
         return True
     else:
         return False
+
+
+# Calculator
+
+def add(n1,n2):
+    return n1 + n2
+
+def subtract(n1,n2):
+    return n1 - n2
+
+def multiply(n1,n2):
+    return n1 * n2
+
+def divide(num1,num2):
+  return num1 / num2
+
+
+# Create a dict where KEYS are the symbols use
+#  VALUES are the result of the functions
+operations = {
+        "+":add,
+        "-":subtract,
+        "*":multiply,
+        "/":divide
+}
+
+num1 = float(input("What's the first number? "))
+num2 = float(input("What's the second number? "))
+op_sym = input("Which operation would you like to perform? ")
+
+
+# for i in operations:
+#     if ask == i:
+#         answer = operations[i]
+#         print(f"{num1} {i} {num2} results in {answer}")
+
+# This gives an unexpected result: 
+# 2 + 3 results in <function add at 0x000001AEE14798A0>
+
+# Task can be ssolved by passing the dictionary key to a function
+calc_func = operations[op_sym]
+answer = calc_func(num1, num2)
+
+print(f"{num1} {op_sym} {num2} results in {answer}")
+
+
+# Another approach to solve by using else if statements
+
+
+# while True:
+    # take input from the user
+    # choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    # if choice in ('1', '2', '3', '4'):
+    #     try:
+    #         num1 = float(input("Enter first number: "))
+    #         num2 = float(input("Enter second number: "))
+    #     except ValueError:
+    #         print("Invalid input. Please enter a number.")
+    #         continue
+
+    #     if choice == '1':
+    #         print(num1, "+", num2, "=", add(num1, num2))
+
+    #     elif choice == '2':
+    #         print(num1, "-", num2, "=", subtract(num1, num2))
+
+    #     elif choice == '3':
+    #         print(num1, "*", num2, "=", multiply(num1, num2))
+
+    #     elif choice == '4':
+    #         print(num1, "/", num2, "=", divide(num1, num2))
+        
+    #     # check if user wants another calculation
+    #     # break the while loop if answer is no
+    #     next_calculation = input("Let's do next calculation? (yes/no): ")
+    #     if next_calculation == "no":
+    #       break
+    # else:
+    #     print("Invalid Input")
+
+
+# RECURSION 
+# You can call a function within the def of the function
+# 
+def calculator():
+    num1 = float(input("Gimme me a number: ")) 
+    for i in operations:
+        print(i)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("Gimme me more: ")) 
+        calc_func = operations[operation_symbol]
+        answer = calc_func(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} result in {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a nwe calculation ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+
--- Day_6.py
+++ Day_6.py
@@ -1,1 +1,34 @@
 # Functions
+
+# Python Built-in Functions.(print() lower() etc : https://docs.python.org/3/library/functions.html)
+# Some useful built-in functions
+# help()
+
+# bool(): return a boolean (True or False)
+# type() : With one argument, return the type of an object.
+# Type conversion
+# float()
+# int()
+# str()
+
+# range(): 
+
+# range is not a func in astrict sense
+# Get a range from 0 to 100 - not inclusive, give 101
+# print(range(100)) 
+# for i in range(101):
+#     print(i)
+# Print every 4th year in a range
+# range(start, stop, step=1): Print every fourth year from 1980 until 2030
+# for i in range(1980, 2030, 4):
+#     print(i)
+
+
+
+# round() : Return number rounded to ndigits precision after the decimal point.
+#           If ndigits is omitted or is None, it returns the nearest integer to its input.
+
+
+# Python Recursion Functions.
+# Python Lambda Functions ( anonymous func)
+# Python User-defined Functions.
