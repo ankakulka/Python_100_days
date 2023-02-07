0f4702fbda4b49188176e2619138c8d9bb943c11
ankakulczycka@gmail.com <ankakulczycka@gmail.com>
2023-02-07 17:21:51 +0100

    Added Day 18 GUI Turtle graphics

---
+++ Day_18_TURTLE_GUI/main.py
@@ -0,0 +1,40 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape('turtle')
+timmy.color('purple')
+# Draw a square using turtle
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen  = Screen()
+screen.exitonclick()
