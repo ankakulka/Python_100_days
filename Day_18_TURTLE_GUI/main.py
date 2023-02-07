c52dea963ee4b5736b20b36fe09e8c0653879368
ankakulczycka@gmail.com <ankakulczycka@gmail.com>
2023-02-07 18:10:59 +0100

    Updated Day 18 GUI Turtle graphics

--- Day_18_TURTLE_GUI/main.py
+++ Day_18_TURTLE_GUI/main.py
@@ -1,20 +1,48 @@
 from turtle import Turtle, Screen
+import heroes
 
 timmy = Turtle()
 timmy.shape('turtle')
 timmy.color('purple')
 # Draw a square using turtle
-timmy.forward(100)
-timmy.left(90)
-timmy.forward(100)
-timmy.left(90)
-timmy.forward(100)
-timmy.left(90)
-timmy.forward(100)
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
+# timmy.left(90)
+# timmy.forward(100)
 
+# draw a dashed line alternating with solid line
+# for _ in range(15):
+#     timmy.forward(10)
+#     timmy.penup()
+#     timmy.forward(10)
+#     timmy.pendown()
 
+# Triangle
+# timmy.forward(100)
+# timmy.right(120)
+# timmy.forward(100)
+# timmy.right(60)
 
+# Triangle
+for i in range(3):
+    timmy.color('red')
+    timmy.forward(100)
+    timmy.right(120)
 
+# Square
+for i in range(4):
+    timmy.color('green')
+    timmy.left(90)
+    timmy.forward(100)
+
+# Pentagon
+for i in range(5):
+    timmy.color('blue')
+    timmy.forward(100)
+    timmy.right(72)
 
 
 
